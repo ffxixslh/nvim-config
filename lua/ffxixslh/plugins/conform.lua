597fad4cd6c05585ac59ac3f8f00f48270f6ef95
luohao <1223048059@qq.com>
2024-09-14 16:53:43 +0800

    chore: update config

--- lua/ffxixslh/plugins/conform.lua
+++ lua/ffxixslh/plugins/conform.lua
@@ -26,55 +26,55 @@ for _, ft in ipairs(eslint_d_ft) do
   formatters_by_ft[ft] = { 'eslint_d' }
 end
 
-  return {
-    'stevearc/conform.nvim',
-    ft = vim.tbl_keys(formatters_by_ft),
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
+return {
+  'stevearc/conform.nvim',
+  ft = vim.tbl_keys(formatters_by_ft),
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
     },
-    config = function()
-      local conform = require 'conform'
-      local utils = require 'ffxixslh.utils'
+  },
+  config = function()
+    local conform = require 'conform'
+    local utils = require 'ffxixslh.utils'
 
-      conform.setup {
-        formatters_by_ft = formatters_by_ft,
-      }
+    conform.setup {
+      formatters_by_ft = formatters_by_ft,
+    }
 
-      utils.config_autocmd('BufWritePre', {
-        pattern = '*',
-        callback = function(e)
-          local filetype = vim.bo[e.buf].filetype
+    utils.config_autocmd('BufWritePre', {
+      pattern = '*',
+      callback = function(e)
+        local filetype = vim.bo[e.buf].filetype
 
-          if not formatters_by_ft[filetype] or vim.g.disable_autoformat then
-            return
-          end
+        if not formatters_by_ft[filetype] or vim.g.disable_autoformat then
+          return
+        end
 
-          local client = vim.lsp.get_clients({ buf = e.buf, name = 'eslint' })[1]
+        local client = vim.lsp.get_clients({ buf = e.buf, name = 'eslint' })[1]
 
-          if client then
-            pcall(vim.lsp.buf.format, {
-              async = false,
-              timeout_ms = 4000,
-            })
-          end
-
-          pcall(conform.format, {
-            bufnr = e.buf,
-            timeout_ms = 1000,
-            lsp_fallback = true,
+        if client then
+          pcall(vim.lsp.buf.format, {
+            async = false,
+            timeout_ms = 4000,
           })
-        end,
-      })
+        end
 
-      vim.api.nvim_create_user_command('FormatToggle', function()
-        vim.g.disable_autoformat = not vim.g.disable_autoformat
-      end, {})
-    end,
-  }
+        pcall(conform.format, {
+          bufnr = e.buf,
+          timeout_ms = 1000,
+          lsp_fallback = true,
+        })
+      end,
+    })
+
+    vim.api.nvim_create_user_command('FormatToggle', function()
+      vim.g.disable_autoformat = not vim.g.disable_autoformat
+    end, {})
+  end,
+}
